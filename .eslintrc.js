6dadc66c3ca2b453c76d0221e45a2ae4239e7751
zhaoyuxing <zyxweb@163.com>
2020-06-22 16:13:10 +0800

    整理文件基本配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'space-before-function-paren': 0,
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'indent': 0,
+    'eol-last': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
