6bc88e79ac3588a8d02a284199c1b1d78ce77053
zhaoyuxing <zyxweb@163.com>
2020-06-27 20:15:40 +0800

    推荐页面的完善

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recommend" ref="recommend">
-    <!-- <scroll ref="scroll" class="recommend-content" :data="discList">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
         <div v-if="recommends.length" class="slider-wrapper">
           <div class="slider-content">
@@ -37,39 +37,142 @@
         <loading></loading>
       </div>
     </scroll>
-    <router-view></router-view>-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-// import Slider from 'base/slider/slider'
-// import Loading from 'base/loading/loading'
-// import Scroll from 'base/scroll/scroll'
-import { getRecommend } from 'api/recommend'
+import Slider from 'base/slider/slider'
+import Loading from 'base/loading/loading'
+import Scroll from 'base/scroll/scroll'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 export default {
-  // components: {
-  //   Slider,
-  //   Loading,
-  //   Scroll
-  // },
+  components: {
+    Slider,
+    Loading,
+    Scroll
+  },
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
+    loadImage() {
+      // 只需要加载完第一张图片后，就可以确定轮播图的高度，然后刷新scroll组件
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        setTimeout(() => {
+          this.$refs.scroll.refresh()
+        }, 20)
+      }
+    },
+    selectItem(item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
     _getRecommend() {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
         }
       })
+    },
+    _getDiscList() {
+      getDiscList().then(res => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
+        }
+      })
     }
   }
 }
 </script>
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+
+  .recommend-content {
+    height: 100%;
+    overflow: hidden;
+
+    .slider-wrapper {
+      position: relative;
+      width: 100%;
+      height: 0;
+      padding-top: 40%;
+      overflow: hidden;
+
+      .slider-content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
+
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+
+          .desc {
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import '@/common/stylus/index.styl'
 
 fastclick.attach(document.body)
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
